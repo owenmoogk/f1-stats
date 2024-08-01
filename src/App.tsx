0ca175f84e228744fd7ffb14e6fc9b0690305658
Owen Moogk <owenmoogk@gmail.com>
2024-08-01 08:56:39 -0400

    update some styles

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,17 @@ function App() {
 
 	return (
 		<div className="App">
-			<p>Note: This webpage will no longer be functional, due to the deprecation of the Ergast F1 API. Until a (free) alternative is found, it will remain unfunctional.</p>
+			<h1>F1 Hypothetical Stats</h1>
+			<h3><a href="https://owenmoogk.github.io/projects/f1-stats">About / Info</a></h3>
+			<p>Note: At the end of 2024, this webpage will no longer be functional, due to the deprecation of the Ergast F1 API. Until a free alternative is found, it will remain unfunctional.</p>
 			{pointsData ?
-				<>
+				<div style={{
+					width: "fit-content",
+					margin: "auto",
+				}}>
 					<Drivers pointsData={pointsData} />
 					<Constructors pointsData={pointsData} />
-				</>
+				</div>
 				: null
 			}
 		</div>
